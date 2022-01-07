41d77e68f70bf6a120326fdb5742e56792caa608
Kwitsch <kwitsch@gmail.com>
2022-01-08 00:24:30 +0100

    added locks

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -3,11 +3,13 @@ package cache
 import (
 	"fmt"
 	"strings"
+	"sync"
 )
 
 type Cache struct {
 	dns  map[string]string
 	rdns map[string]string
+	lock sync.RWMutex
 }
 
 func New() *Cache {
@@ -19,28 +21,37 @@ func New() *Cache {
 
 func (c *Cache) Update(hostname, ip string) {
 	c.Delete(hostname)
+
 	if revIp, revErr := reverseIP(ip); revErr == nil {
+		c.lock.Lock()
 		c.dns[hostname] = ip
 		c.rdns[revIp] = hostname
+		c.lock.Unlock()
 	}
 }
 
 func (c *Cache) Delete(hostname string) {
 	if oip, dok := c.dns[hostname]; dok {
+		c.lock.Lock()
 		delete(c.dns, hostname)
 		revIp, _ := reverseIP(oip)
 		if _, rok := c.rdns[revIp]; rok {
 			delete(c.rdns, revIp)
 		}
+		c.lock.Unlock()
 	}
 }
 
 func (c *Cache) GetIp(hostname string) (string, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	ip, ok := c.dns[hostname]
 	return ip, ok
 }
 
 func (c *Cache) GetHostname(reverseIP string) (string, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	hostname, ok := c.rdns[reverseIP]
 	return hostname, ok
 }
