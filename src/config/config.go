52500b5051dc0edefc5f07c4ef5fc2679906280b
Kwitsch <kwitsch@gmail.com>
2022-01-07 22:23:45 +0100

    more config values

--- src/config/config.go
+++ src/config/config.go
@@ -7,18 +7,24 @@ import (
 )
 
 type Config struct {
-	Redis   RedisConfig `koanf:"redis"`
-	Verbose bool        `koanf:"verbose" default:"false"`
+	Redis   RedisConfig           `koanf:"redis"`
+	Hosts   map[string]HostConfig `koanf:"hosts"`
+	Verbose bool                  `koanf:"verbose" default:"false"`
 }
 
 type RedisConfig struct {
-	Address  string        `koanf:"address"`
-	Username string        `koanf:"username"`
-	Password string        `koanf:"password"`
-	Database int           `koanf:"database" default:"0"`
-	Attempts int           `koanf:"attempts" default:"3"`
-	Cooldown time.Duration `koanf:"cooldown" default:"1s"`
-	Verbose  bool
+	Address   string        `koanf:"address"`
+	Username  string        `koanf:"username"`
+	Password  string        `koanf:"password"`
+	Database  int           `koanf:"database" default:"0"`
+	Attempts  int           `koanf:"attempts" default:"3"`
+	Cooldown  time.Duration `koanf:"cooldown" default:"1s"`
+	Intervall time.Duration `koan:"intervall" default:"5m"`
+	Verbose   bool
+}
+
+type HostConfig struct {
+	Mac map[int]string `koanf:"mac"`
 }
 
 const prefix = "TMD_"
