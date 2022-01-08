e807b962835e287396b1e13a17433115a84ced5e
Kwitsch <kwitsch@gmail.com>
2022-01-08 04:14:28 +0100

    added dns server

--- src/go.mod
+++ src/go.mod
@@ -8,10 +8,19 @@ require (
 )
 
 require (
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/creasty/defaults v1.5.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/knadh/koanf v1.3.3 // indirect
+	github.com/miekg/dns v1.1.45
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
---
+++ src/server/server.go
@@ -0,0 +1,99 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/kwitsch/TinyMacDns/cache"
+	"github.com/miekg/dns"
+)
+
+type Server struct {
+	udp   *dns.Server
+	cache *cache.Cache
+	ttl   uint32
+}
+
+func New(cache *cache.Cache, ttl int) *Server {
+	res := &Server{
+		udp:   createUDPServer(),
+		cache: cache,
+		ttl:   uint32(ttl),
+	}
+
+	handler := res.udp.Handler.(*dns.ServeMux)
+	handler.HandleFunc(".", res.OnRequest)
+
+	return res
+}
+
+func (s *Server) Start() error {
+	return s.udp.ListenAndServe()
+}
+
+func createUDPServer() *dns.Server {
+	return &dns.Server{
+		Addr:    "53",
+		Net:     "udp",
+		Handler: dns.NewServeMux(),
+		NotifyStartedFunc: func() {
+			fmt.Println("UDP server is up and running")
+		},
+		UDPSize: 65535,
+	}
+}
+
+const rdnsSuf string = ".in-addr.arpa"
+
+func (s *Server) OnRequest(w dns.ResponseWriter, request *dns.Msg) {
+	q := request.Question[0]
+	m := new(dns.Msg)
+	m.SetReply(request)
+	if q.Qtype == dns.TypePTR || q.Qtype == dns.TypeA {
+		cname := strings.TrimSuffix(strings.ToLower(q.Name), ".")
+		exists := false
+		val := ""
+
+		if q.Qtype == dns.TypePTR {
+			crname := strings.TrimSuffix(cname, rdnsSuf)
+
+			val, exists = s.cache.GetHostname(crname)
+			if exists {
+				rr := new(dns.PTR)
+				rr.Hdr = dns.RR_Header{
+					Name:   q.Name,
+					Rrtype: dns.TypePTR,
+					Class:  dns.ClassINET,
+					Ttl:    s.ttl,
+				}
+
+				rr.Ptr = val
+
+				m.Answer = []dns.RR{rr}
+			}
+		} else if q.Qtype == dns.TypeA {
+			val, exists = s.cache.GetIp(cname)
+			if exists {
+				rr := new(dns.A)
+				rr.Hdr = dns.RR_Header{
+					Name:   q.Name,
+					Rrtype: dns.TypeA,
+					Class:  dns.ClassINET,
+					Ttl:    s.ttl,
+				}
+
+				rr.A = net.ParseIP(val)
+
+				m.Answer = []dns.RR{rr}
+			}
+		}
+
+		if !exists {
+			m.SetRcode(request, dns.RcodeNameError)
+		}
+	} else {
+		m.SetRcode(request, dns.RcodeRefused)
+	}
+	w.WriteMsg(m)
+}
